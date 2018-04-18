4e29da41dc2529ece108016987a99662ba16cf7a
Leonardo Daniel Chipres Arteaga <33239864+lchipres@users.noreply.github.com>
2018-04-18 16:07:29 -0500

    Add files via upload

---
+++ Catalogo/Catalogo/Catal.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catalogo
+{
+    class Catal : Producto
+    {
+        Producto[] p = new Producto[15];
+        int cuantosBan = 0;
+
+        public void agregarP(Producto p)
+        {
+            if (cuantosBan > 14)
+            {
+                cuantosBan = 0;
+            }
+            this.p[cuantosBan] = p;
+            cuantosBan++;
+        }
+
+        public void insertarP(Producto p,int pos)
+        {
+            if (cuantosBan > 14)
+            {
+                cuantosBan = 0;
+            }
+            this.p[pos - 1] = p;
+            cuantosBan++;   
+        }
+
+        public int getCuantos()
+        {
+            return cuantosBan;
+        }
+
+        public override string ToString()
+        {
+            string s = "";
+            foreach (Producto element in p)
+            {
+                s += element+Environment.NewLine;
+            }
+            return s;
+        }
+
+        public string buscarP(int posicion)
+        {
+            string s = "";
+            Producto pBus;
+            pBus = p[posicion];
+            s = pBus+Environment.NewLine;
+            return s;
+        }
+
+        public void eliminarP(int posicion)
+        {
+            p[posicion] = null;
+        }
+    }
+}
---
+++ Catalogo/Catalogo/Producto.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catalogo
+{
+    class Producto
+    {
+        protected int codigo,cantidad,id;
+        protected string nombre;
+        protected double costo;
+
+        public Producto(int id,int codigo, int cantidad, string nombre, double costo)
+        {
+            this.codigo = codigo;
+            this.cantidad = cantidad;
+            this.nombre = nombre;
+            this.costo = costo;
+            this.id = id;
+        }
+
+        public Producto()
+        {
+
+        }
+
+        public int getCodigo()
+        {
+            return codigo;
+        }
+
+        public int getCantidad()
+        {
+            return cantidad;
+        }
+
+        public string getNombre()
+        {
+            return nombre;
+        }
+
+        public double getCosto()
+        {
+            return costo;
+        }
+
+        public int getID()
+        {
+            return id;
+        }
+
+        public void setID(int id)
+        {
+            this.id=id;
+        }
+
+        public override string ToString()
+        {
+            string s = "Id de producto: " + id + " Codigo: " + codigo + " Nombre: " + nombre + " Cantidad: " + cantidad + " Costo: "+costo;
+            return s;
+        }
+
+    }
+}
